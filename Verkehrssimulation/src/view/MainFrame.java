ea828ce6310a21fc680dc18fef4226cec426cc19
uzapy <bublm1@bfh.ch>
2014-10-01 20:46:00 +0200

    Das Auto bewegt sich

--- Verkehrssimulation/src/view/MainFrame.java
+++ Verkehrssimulation/src/view/MainFrame.java
@@ -7,24 +7,40 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
+import timer.IImpulsable;
+import timer.Impulse;
+
+/**
+ * @author bublm1
+ */
 @SuppressWarnings("serial")
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements IImpulsable {
+	
+	private CarPanel car1 = new CarPanel();
+	private Impulse impulse;
 	
 	public MainFrame(String title) {
 		super(title);
+		this.impulse = new Impulse(this);		
 
 		// Layout manager
 		setLayout(new BorderLayout());
 
 		// Swing components
-		JTextArea textArea = new JTextArea();
 		JButton button = new JButton("Click me!");
-
+		
 		// Add Swing Components
 		Container container = getContentPane();
-		container.add(textArea, BorderLayout.CENTER);
+		container.add(car1, BorderLayout.CENTER);
 		container.add(button, BorderLayout.SOUTH);
-		
 		// Tutorial: http://www.youtube.com/watch?v=svM0SBFqp4s
+	}
+
+	/* (non-Javadoc)
+	 * @see timer.IImpulsable#poke()
+	 */
+	@Override
+	public void poke() {
+		car1.moveForward();
 	}
 }
