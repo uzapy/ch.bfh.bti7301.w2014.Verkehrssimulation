fad22275c07216887f58163947393f283d1733f3
burkt4 <burkt4@bfh.ch>
2014-10-29 22:24:35 +0100

    1. Versuch für rechts überholen verhindern

--- Verkehrssimulation/src/model/Nagel_Schreckenberg_Simulation.java
+++ Verkehrssimulation/src/model/Nagel_Schreckenberg_Simulation.java
@@ -14,7 +14,7 @@ public class Nagel_Schreckenberg_Simulation {
 
 	public static int FRAMES_PER_SECOND = 30;	// Simulationsgeschwindigkeit in Frames pro Sekunde
 	private Track track;
-	private int speedDelta = 5; 				// Standardbeschleunigung in Meter pro Sekunde
+	private int speedDelta = 3; 				// Standardbeschleunigung in Meter pro Sekunde
 	private double trödelFactor = 0.3;
 	private int securityDistance = 1;
 	ArrayList<Car> carList = new ArrayList<Car>();
@@ -26,18 +26,18 @@ public class Nagel_Schreckenberg_Simulation {
 		Lane lane0 = new Lane(20, 150, 0);
 		Lane lane1 = new Lane(20, 150, 1);
 		Lane lane2 = new Lane(20, 150, 2);
-		Lane lane3 = new Lane(20, 150, 3);
+//		Lane lane3 = new Lane(20, 150, 3);
 		
 		lane0.setAdjacentLanes(lane1, null);
 		lane1.setAdjacentLanes(lane2, lane0);
-		lane2.setAdjacentLanes(lane3, lane1);
-		lane3.setAdjacentLanes(null,  lane2);
+		lane2.setAdjacentLanes(null, lane1);
+//		lane3.setAdjacentLanes(null,  lane2);
 
 		this.track = new Track();
 		this.track.addLane(lane0);
 		this.track.addLane(lane1);
 		this.track.addLane(lane2);
-		this.track.addLane(lane3);
+//		this.track.addLane(lane3);
 		
 		lane0.addCar(new Car( 1, 1, trödelFactor,  9, RandomPool.getNewCarLength(), lane0));
 		lane0.addCar(new Car( 2, 3, trödelFactor, 37, RandomPool.getNewCarLength(), lane0));
@@ -79,6 +79,7 @@ public class Nagel_Schreckenberg_Simulation {
 				int speedOnFastLane = getPossibleMaximumSpeed(lane.getLeftLane(), car);
 				int speedOnSlowLane = getPossibleMaximumSpeed(lane.getRightLane(), car);
 				int speedOnCurrentLane = getPossibleMaximumSpeed(lane, car);
+				
 				
 				Car previousCar = null;
 				// Soll das Auto auto auf die rechtere Spur wechseln?
@@ -216,6 +217,27 @@ public class Nagel_Schreckenberg_Simulation {
 				speed = setSpeedAccordingToAvaliableSpace(speed, availableSpace);
 			}
 		}
+		
+		// rechts ÜBERHOLEN CHECK
+		if (lane.isPassableLeft()){
+			Lane leftLane = lane.getLeftLane();
+			if (!(leftLane.getNextCar(car) == null)) {
+					int speedNextLeftCar = leftLane.getNextCar(car).getSpeed();
+					if ((car.getPosition() + speed) % lane.getLength() > (leftLane.getNextCar(car).getPosition() + speedNextLeftCar) % leftLane.getLength()){
+						speed = speedNextLeftCar;
+					}
+			// Spur zu Ende
+			} else {
+				Car firstCar = leftLane.getFirstCar();
+				if (firstCar != null) {
+					int speedNextLeftCar = firstCar.getSpeed();
+					if ((car.getPosition() + speed) % lane.getLength() >  (firstCar.getPosition() + speedNextLeftCar) % leftLane.getLength()){
+						speed = speedNextLeftCar;
+					}
+				}
+			}
+		}
+		
 
 		return speed;
 	}
