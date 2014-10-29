25021979b4a7ef495057fb4bb069a931f66a3228
uzapy <bublm1@bfh.ch>
2014-10-29 18:59:33 +0100

    More Random initial Values

--- Verkehrssimulation/src/model/Nagel_Schreckenberg_Simulation.java
+++ Verkehrssimulation/src/model/Nagel_Schreckenberg_Simulation.java
@@ -14,7 +14,7 @@ public class Nagel_Schreckenberg_Simulation {
 
 	public static int FRAMES_PER_SECOND = 30;	// Simulationsgeschwindigkeit in Frames pro Sekunde
 	private Track track;
-	private int speedDelta = 5; 				// Standardbeschleunigung in Meter pro Sekunde
+	private int speedDelta = 3; 				// Standardbeschleunigung in Meter pro Sekunde
 	private double trödelFactor = 0.3;
 	private int securityDistance = 1;
 	ArrayList<Car> carList = new ArrayList<Car>();
@@ -26,38 +26,38 @@ public class Nagel_Schreckenberg_Simulation {
 		Lane lane0 = new Lane(15, 150, 0);
 		Lane lane1 = new Lane(15, 150, 1);
 		Lane lane2 = new Lane(15, 150, 2);
-		Lane lane3 = new Lane(15, 150, 3);
+//		Lane lane3 = new Lane(15, 150, 3);
 		
 		lane0.setAdjacentLanes(lane1, null);
 		lane1.setAdjacentLanes(lane2, lane0);
-		lane2.setAdjacentLanes(lane3, lane1);
-		lane3.setAdjacentLanes(null,  lane2);
+		lane2.setAdjacentLanes(null, lane1);
+//		lane3.setAdjacentLanes(null,  lane2);
 
 		this.track = new Track();
 		this.track.addLane(lane0);
 		this.track.addLane(lane1);
 		this.track.addLane(lane2);
-		this.track.addLane(lane3);
+//		this.track.addLane(lane3);
 		
-		lane0.addCar(new Car(1, 0, trödelFactor, 0,  RandomPool.getNewCarLength(), lane0));
-		lane0.addCar(new Car(2, 0, trödelFactor, 30, RandomPool.getNewCarLength(), lane0));
-		lane0.addCar(new Car(3, 0, trödelFactor, 40, RandomPool.getNewCarLength(), lane0));
-		lane0.addCar(new Car(4, 0, trödelFactor, 60, RandomPool.getNewCarLength(), lane0));
-		lane0.addCar(new Car(5, 0, trödelFactor, 90, RandomPool.getNewCarLength(), lane0));
+		lane0.addCar(new Car(1, 1, trödelFactor, 9,  RandomPool.getNewCarLength(), lane0));
+		lane0.addCar(new Car(2, 3, trödelFactor, 37, RandomPool.getNewCarLength(), lane0));
+		lane0.addCar(new Car(3, 5, trödelFactor, 68, RandomPool.getNewCarLength(), lane0));
+		lane0.addCar(new Car(4, 7, trödelFactor, 95, RandomPool.getNewCarLength(), lane0));
+		lane0.addCar(new Car(5, 9, trödelFactor, 40, RandomPool.getNewCarLength(), lane0));
 	
-		lane1.addCar(new Car(6, 0, trödelFactor, 10, RandomPool.getNewCarLength(), lane1));
-		lane1.addCar(new Car(7, 0, trödelFactor, 40, RandomPool.getNewCarLength(), lane1));
-		lane1.addCar(new Car(8, 0, trödelFactor, 50, RandomPool.getNewCarLength(), lane1));
-		lane1.addCar(new Car(9, 0, trödelFactor, 60, RandomPool.getNewCarLength(), lane1));
-		lane1.addCar(new Car(10,0, trödelFactor, 80, RandomPool.getNewCarLength(), lane1));
+		lane1.addCar(new Car(6, 11, trödelFactor, 43, RandomPool.getNewCarLength(), lane1));
+		lane1.addCar(new Car(7, 13, trödelFactor, 54, RandomPool.getNewCarLength(), lane1));
+		lane1.addCar(new Car(8, 15, trödelFactor, 62, RandomPool.getNewCarLength(), lane1));
+		lane1.addCar(new Car(9, 0, trödelFactor, 14, RandomPool.getNewCarLength(), lane1));
+		lane1.addCar(new Car(10,2, trödelFactor, 97, RandomPool.getNewCarLength(), lane1));
 
-		lane2.addCar(new Car(11, 0, trödelFactor, 10, RandomPool.getNewCarLength(), lane2));
-		lane2.addCar(new Car(12, 0, trödelFactor, 40, RandomPool.getNewCarLength(), lane2));
-		lane2.addCar(new Car(13, 0, trödelFactor, 50, RandomPool.getNewCarLength(), lane2));
-		lane2.addCar(new Car(14, 0, trödelFactor, 60, RandomPool.getNewCarLength(), lane2));
-		lane2.addCar(new Car(15, 0, trödelFactor, 80, RandomPool.getNewCarLength(), lane2));
+		lane2.addCar(new Car(11, 4, trödelFactor, 25, RandomPool.getNewCarLength(), lane2));
+		lane2.addCar(new Car(12, 6, trödelFactor, 85, RandomPool.getNewCarLength(), lane2));
+		lane2.addCar(new Car(13, 8, trödelFactor, 52, RandomPool.getNewCarLength(), lane2));
+		lane2.addCar(new Car(14, 10, trödelFactor, 4, RandomPool.getNewCarLength(), lane2));
+		lane2.addCar(new Car(15, 12, trödelFactor, 26, RandomPool.getNewCarLength(), lane2));
+		lane2.addCar(new Car(16, 14, trödelFactor, 71, RandomPool.getNewCarLength(), lane2));
 
-		lane3.addCar(new Car(15, 0, trödelFactor, 80, RandomPool.getNewCarLength(), lane3));
 	}
 
 	public Track performStep() {
