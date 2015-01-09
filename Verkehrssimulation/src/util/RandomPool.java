ad6a2098be54b7d9d1ab8feec442c04033d299aa
Reto Stähli <staehli.reto@gmail.com>
2015-01-09 14:49:08 +0100

    RandomPool kommentiert

--- Verkehrssimulation/src/util/RandomPool.java
+++ Verkehrssimulation/src/util/RandomPool.java
@@ -10,7 +10,8 @@ import model.Car;
 import model.Lane;
 
 /**
- * @author bublm1
+ * Klasse welche Zufallswerte, welche in der Simulation gebraucht werden, produziert
+ * @author bublm1, stahr2, burkt4
  */
 public class RandomPool {
 
@@ -23,8 +24,9 @@ public class RandomPool {
 	private RandomPool() { }
 
 	/**
+	 * Generiert eine neue zufällige Farbe
 	 * @author bublm1
-	 * @return
+	 * @return Color:	Neue zufällige Farbe
 	 */
 	public static Color getNewColor() {
 		return new Color(r.nextFloat(), r.nextFloat(), r.nextFloat());
@@ -33,7 +35,12 @@ public class RandomPool {
 	public static double nextDouble() {
 		return r.nextDouble();
 	}
-
+	/**
+	 * Generiert ein neues zufälliges Auto
+	 * @author stahr2
+	 * @param lane:		Die Fahrspur, auf welcher das Auto generiert werden soll
+	 * @return car:		Neues zufälliges Auto
+	 */
 	public static Car getNewCar(Lane lane){
 		int speed = getNewCarSpeed(lane.getMaxVelocity(0));
 		double trödelFactor = getNewTrödelFactor();
@@ -42,19 +49,32 @@ public class RandomPool {
 		return new Car(id++, speed, trödelFactor, 0, length, lane);
 	}
 	
+	/**
+	 * Generiert eine neue zufällige Geschwindigkeit
+	 * @author burkt4
+	 * @return speed:	Neue zufällige Geschwindigkeit
+	 */
+	
 	public static int getNewCarSpeed(int maxVelocity) {
 		int fastest = maxVelocity;
 		int slowest = 3;
 		return r.nextInt(fastest - slowest) + slowest;
 	}
 	
+	/**
+	 * Generiert einen neuen zufälligen Trödelfaktor
+	 * @author burkt4
+	 * @return speed:	Neuer zufälliger Tröderlfaktor
+	 */
+	
 	public static double getNewTrödelFactor() {
 		return r.nextDouble() * (0.5 - 0.2) + 0.2;
 	}
 	
 	/**
+	 * Generiert eine neue zufällige Länge eines Autos
 	 * @author bublm1
-	 * @return
+	 * @return lenght:	Neue zufällige Länge
 	 */
 	public static int getNewCarLength() {
 		int shortest = 3;
